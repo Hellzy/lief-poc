1030fb879e6d0e9818112d0c3d7ab88c903c1f38
Francois Mazeau <frmazeau2@gmail.com>
2024-06-14 08:15:30 +0200

    Bin rewriting for go app

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,29 @@
-#include <LIEF/LIEF.hpp>
+#include <algorithm>
 #include <iostream>
+#include <utility>
+#include <vector>
+
+#include <LIEF/LIEF.hpp>
 
 using binPtr_t = std::unique_ptr<LIEF::ELF::Binary>;
 
-void substituteCall(binPtr_t& bin, const std::string& sym1_name, const std::string& sym2_name) {
+void listSyms(const binPtr_t& bin) {
+    for (const auto& sym : bin->symbols())
+        std::cout << sym.name() << '\n';
+}
+
+bool substituteCall(binPtr_t& bin, const std::string& sym1_name, const std::string& sym2_name) {
     auto* sym1 = bin->get_symbol(sym1_name);
     auto* sym2 = bin->get_symbol(sym2_name);
+
+    if (!sym1) {
+        std::cerr << "Couldn't find '" << sym1_name << "'\n";
+        return false;
+    }
+    if (!sym2) {
+        std::cerr << "Couldn't find '" << sym2_name << "'\n";
+        return false;
+    }
 
 
     auto* text = bin->get_section(".text");
@@ -28,14 +46,41 @@ void substituteCall(binPtr_t& bin, const std::string& sym1_name, const std::stri
         }
     }
 
+    return true;
+
+}
+
+void hookGot(binPtr_t& bin, const std::string& sym1_name, const std::string& sym2_name) {
+    auto* got = bin->get_section(".plt.got");
+    if (!got) {
+        return;
+    }
+    auto sym2 =  bin->get_symbol(sym2_name);
+    bin->patch_pltgot(sym1_name, sym2->value());
+}
+
+void switchSyms(binPtr_t& bin, const std::string& sym1_name, const std::string& sym2_name) {
+    auto* sym1 = bin->get_symtab_symbol(sym1_name);
+    auto* sym2 = bin->get_symtab_symbol(sym2_name);
+
+    bin->remove_symtab_symbol(sym1_name);
+    bin->remove_symtab_symbol(sym2_name);
+
 }
 
 int main() {
-    auto bin = LIEF::ELF::Parser::parse("../bin/hello");
-    std::string sym_name1 = "_Z5hellov";
-    std::string sym_name2 = "_Z7goodbyev";
+    //auto bin = LIEF::ELF::Parser::parse("../bin/hello");
+    //std::string sym_name1 = "_Z5hellov";
+    //std::string sym_name2 = "_Z7goodbyev";
 
-    substituteCall(bin, sym_name1, sym_name2);
+    auto bin = LIEF::ELF::Parser::parse("../go/app");
+    std::string sym_name1 = "math/rand.Int";
+    std::string sym_name2 = "main.Int";
+
+    //hookGot(bin, sym_name1, sym_name2);
+    //switchSyms(bin, sym_name1, sym_name2);
+    if (!substituteCall(bin, sym_name1, sym_name2))
+        listSyms(bin);
 
     LIEF::ELF::Builder builder(*bin);
     builder.build();
